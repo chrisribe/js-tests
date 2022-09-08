5cae1e641d3edc881c4573725bb7b6f47667dfd8
Ribe, Christian <Christian.Ribe@nuance.com>
2022-09-08 16:10:27 -0400

    main Added some formatting and buttons

--- bfcache/ui-toggle.js
+++ bfcache/ui-toggle.js
@@ -2,29 +2,30 @@ var uiToggleTest = function(){
     var intervalTM = null;
     var toggleFlag = false;
     function uiDisableEnableToggle(target) {
-      toggleFlag = !toggleFlag;
-      if(toggleFlag) {
-        target.setAttribute('disabled', 'true');
-      } else {
-        target.removeAttribute('disabled')
-      }
-      console.log(`uiToggleTest running ${toggleFlag}, for ${target.tagName}`);
+        toggleFlag = !toggleFlag;
+        if(toggleFlag) {
+            target.setAttribute('disabled', 'true');
+        } else {
+            target.removeAttribute('disabled')
+        }
+        console.log(`uiToggleTest running ${toggleFlag}, for ${target.tagName}`);
     }
 
     function startInterval(selectorString) {
-      intervalTM = setInterval(function(){
-        uiDisableEnableToggle(
-          document.body.querySelector(selectorString)
-        );
-      }, 3000);
+        stop();
+        intervalTM = setInterval(function(){
+            uiDisableEnableToggle(
+                document.body.querySelector(selectorString)
+            );
+        }, 3000);
     }
     function stop(){
-      if(intervalTM) {
-        clearInterval(intervalTM);
-      }
+        if(intervalTM) {
+            clearInterval(intervalTM);
+        }
     }
     return{
-      start: startInterval,
-      stop: stop
+        start: startInterval,
+        stop: stop
     }
-  }();
+}();
